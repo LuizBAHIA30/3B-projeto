13b6ae8f7ccae699fd276a7e219aff9daf7c4fbe
Luiz Fernando <141454919+LuizBAHIA30@users.noreply.github.com>
2025-04-25 11:43:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo")
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo01 = new Date("2025-05-10T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].texContent = tempoObjetivo01 - tempoAtual
 
 for (let i= 0; i < botoes.length; i++){
 
