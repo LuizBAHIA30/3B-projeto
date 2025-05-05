3ca9345aa95b678fcbe6b494a317d2a4514607c1
Luiz Fernando <141454919+LuizBAHIA30@users.noreply.github.com>
2025-05-05 08:35:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
+
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 const contadores = document.querySelectorAll(".contador");
 
-const tempoObjetivo01 = new Date("2025-04-15T00:00:00");
-const tempoObjetivo02 = new Date("2025-06-05T00:00:00");
+const tempoObjetivo01 = new Date("2025-06-15T00:00:00");
+const tempoObjetivo02 = new Date("2025-08-05T00:00:00");
 const tempoObjetivo03 = new Date("2025-07-02T00:00:00");
-const tempoObjetivo04 = new Date("2025-11-10T00:00:00");
+const tempoObjetivo04 = new Date("2025-05-10T00:00:00");
 
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
-for(i =0; i < contadores.length; i++){
+function atualizaCronometro(){
+for(i =0; i< contadores.length; i++){
     contadores[i].textContent = calculaTempo(tempos[i]);
 }
- 
+}
 
 
 for (let i= 0; i < botoes.length; i++){
@@ -34,7 +36,7 @@ function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
 
-    let segundos = Math.floor(tempoFinal/1000);
+    let segundos = Math.floor(tempoFinal/100);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
@@ -42,6 +44,11 @@ function calculaTempo(tempoObjetivo){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
+    
+    if(tempoFinal > 0)
     return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
+     return "Objetivo Concluido"
 }
+
+  atualizaCronometro();
+  setInterval(atualizaCronometro,100);
